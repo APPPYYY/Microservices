0ae660fd43b550f915a1d3e2fb08430ec7da5ef4
SUBHAM <115138867+APPPYYY@users.noreply.github.com>
2024-06-16 14:56:09 +0530

    Delete UserService directory

--- UserService/src/main/java/com/ms/user/service/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ms.user.service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ms.user.service.entities.User;
-
-public interface UserRepo extends JpaRepository<User, String> {
-
-}
