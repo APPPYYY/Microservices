1be99ceeb46902cfa09186aa4edd5fa5776918d3
SUBHAM <115138867+APPPYYY@users.noreply.github.com>
2024-06-16 15:09:17 +0530

    Add files via upload

---
+++ APIGateway/src/main/java/com/ms/api/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ms.api.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		httpSecurity
+			.authorizeExchange()
+			.anyExchange()
+			.authenticated()
+			.and()
+			.oauth2Client()
+			.and().
+			oauth2ResourceServer()
+			.jwt();
+		
+		return httpSecurity.build();
+	}
+}
