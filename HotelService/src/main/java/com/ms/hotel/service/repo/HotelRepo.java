b2a183bed299ec538ea234aa294469449916fcab
SUBHAM <115138867+APPPYYY@users.noreply.github.com>
2024-06-16 14:56:39 +0530

    Delete HotelService directory

--- HotelService/src/main/java/com/ms/hotel/service/repo/HotelRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ms.hotel.service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ms.hotel.service.entities.Hotel;
-
-public interface HotelRepo extends JpaRepository<Hotel ,String> {
-
-}
